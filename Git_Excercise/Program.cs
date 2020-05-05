cf4f2ee847ddc026adf17818be130c8405a5fa9a
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-05-05 14:54:59 +0200

    Add project files.

---
+++ Git_Excercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
